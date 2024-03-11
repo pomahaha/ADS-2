386ace4aab1c7d1f7d601484ac4d79559f270f6f
pomahaha <110399575+pomahaha@users.noreply.github.com>
2024-03-11 22:27:12 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,26 +3,45 @@
 #include "alg.h"
 
 
-double pown(double value, uint16_t n) {
-
+double pown(double value, uint16_t n) { 
+    double a = 1; 
+    for (uint64_t i=0;i<n;i++) { 
+        a=a*value;} 
+    return a; 
 }
 
-uint64_t fact(uint16_t n) {
-
+uint64_t fact(uint16_t n) { 
+  uint64_t ed=1; 
+  for(uint64_t i=1;i<=n;i++) { 
+      ed=ed*i; 
+  } 
+  return ed; 
 }
 
-double calcItem(double x, uint16_t n) {
-
+double calcItem(double x, uint16_t n) { 
+return (pown(x,n)/fact(n)); 
 }
 
-double expn(double x, uint16_t count) {
-
+double expn(double x, uint16_t count) { 
+double a=0; 
+    for (uint64_t i=0; i<=count;i++) { 
+        a=a+(pown(x,i)/fact(i)); 
+    } 
+    return a; 
 }
 
-double sinn(double x, uint16_t count) {
-
+double sinn(double x, uint16_t count) { 
+    double ed=a; 
+    for (uint16_t b=2;b<=count;++b) { 
+        ed=ed+pown((-1),b-1)*calcItem(x,(2*b)-1); 
+    } 
+    return ed; 
 }
 
-double cosn(double x, uint16_t count) {
-
+double cosn(double x, uint16_t count) { 
+    double ed=1; 
+    for (uint16_t i=2;i<=count;i++) { 
+        ed=ed+pown((-1),i-1)*calcItem(x,(2*i)-2); 
+    } 
+    return ed; 
 }
